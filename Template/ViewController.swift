8ddda16de562784c642971e7dbb3f282c4f17e4a
Arthur Papailhau <papailhau@uber.com>
2021-02-13 16:27:30 +0100

    Initial Commit

---
+++ Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by Arthur Papailhau on 2/13/21.
+//  Copyright © 2021 Arthur Papailhau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
